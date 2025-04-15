2c3eb31fabeb63f98f0d94d7a05f652d225b8294
Laszlo Toth <32448562+pachel@users.noreply.github.com>
2025-04-15 14:42:38 +0200

    Reactos cucc jav.

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import UserManager from "./components/UserManager";
+import Calculator from "./components/calculator";
+import TicTacToe from "./components/TicTacToe";
 
 function App() {
   return (
     <div className="App">
-      <UserManager />
+        <TicTacToe />
+        <Calculator />
     </div>
   );
 }
 
 export default App;
+
